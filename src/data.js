b0fb6c34b9134279abefa29e8920b43ce4ef3a9d
Augusto de Carvalho <67812938+CarvalhoHS@users.noreply.github.com>
2022-06-23 17:35:12 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,30 @@
+
+let shopItemsData = [{
+    id:"Maneiro",
+    name:"Vamo",
+    price:37,
+    desc: "Lorem ipsum dolor",
+    img: "alo.png"
+
+},{
+    id:"Top",
+    name:"Vamo",
+    price:47,
+    desc: "Lorem ipsum dolor",
+    img: "alo.png"
+
+},{
+    id:"Shoppingtop",
+    name:"Vamo",
+    price:57,
+    desc: "Lorem ipsum dolor",
+    img: "alo.png"
+
+},{
+    id:"Demais",
+    name:"Vamo",
+    price:87,
+    desc: "Lorem ipsum dolor",
+    img: "alo.png"
+
+}]
